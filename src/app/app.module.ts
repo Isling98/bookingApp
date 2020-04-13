a1cca8bd5c427190748879a3638d7a8f300c096d
isling98 <s174879@student.dtu.dk>
2020-04-13 15:30:40 +0200

    Lavet page-not-found + redirect til denne side hvis path ikke findes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { LokaleBookingComponent } from './lokale-booking/lokale-booking.componen
 import {RouterModule, Routes} from '@angular/router';
 import { NavigationbarComponent } from './navigationbar/navigationbar.component';
 import { MybookingsComponent } from './mybookings/mybookings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: LoginScreenComponent},
   {path: 'booking', component: LokaleBookingComponent},
-  {path: 'minebookings', component: MybookingsComponent}
+  {path: 'minebookings', component: MybookingsComponent},
+  {path: 'not-found', component: PageNotFoundComponent},
+  /*Nedenstående path skal altid være den nederste path, ellers overwriter den de andre paths og vil altid redirecte!*/
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -22,7 +26,8 @@ const appRoutes: Routes = [
     LoginScreenComponent,
     LokaleBookingComponent,
     NavigationbarComponent,
-    MybookingsComponent
+    MybookingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

521b6c42d2cdedac9577bd0118c885330a74485b
Hemsen <niko.hemsen@gmail.com>
2020-04-19 18:00:56 +0200

    Tilføjet dividers - arbejder videre med Get Request - venter på at endpoint bliver til HTTPS

--- src/app/brugeroplysninger/brugeroplysninger.component.ts
+++ src/app/brugeroplysninger/brugeroplysninger.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Bruger} from './bruger.model';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-bruger-oplysninger',
@@ -11,21 +11,35 @@ export class BrugeroplysningerComponent implements OnInit {
 
   // Dummy data - skal selvfølgelig hente det rigtige senere..
   public bruger: Bruger = new Bruger('s123456', 's123456@student.dtu.dk',
-    '1585136654001', 'ukendt', 'demobruger', 'Dennis',
+    '21:22', 'ukendt', 'demobruger', 'Dennis',
   'Demostudent', '<IKKE OFFENTLIG>', 'Tennis og programmering',
     'http://www.diplom.dtu.dk/');
 
   public testData: JSON;
+  public testBruger: JSON;
+
+  // Den nye kan findes her: http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/
+  // hvor i kan logge ind med username s180077 pass 123
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    /*const headers = {Username: 's180077', Password: '123'}
-    this.http.get<any>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/users/19',
-      {headers}).subscribe
-    (data => {this.testData = data.total;
+    const username = 's180077';
+    const password = '123';
+
+    const authorizationData = 'Basic ' + btoa(username + ':' + password);
+
+    const headerOptions = {
+      headers: new HttpHeaders({
+        Authorization: authorizationData
+      })
+    };
+
+    this.http.get<JSON>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/users',
+      {headers: headerOptions.headers}).subscribe
+    (data => {this.testBruger = data;
     });
-    console.log(this.testData);*/
+    console.log(this.testBruger);
 
     // Test med et anden API
     this.http.get<JSON>('https://random-word-api.herokuapp.com/word?number=10').subscribe

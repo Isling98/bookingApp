f4e428f8f23aa3ed5ff990965d85283e7edab151
Hemsen <niko.hemsen@gmail.com>
2020-04-19 15:35:37 +0200

    Bug fix

--- src/app/brugeroplysninger/brugeroplysninger.component.ts
+++ src/app/brugeroplysninger/brugeroplysninger.component.ts
@@ -10,17 +10,19 @@ import { HttpClient } from '@angular/common/http';
 export class BrugeroplysningerComponent implements OnInit {
 
   // Dummy data - skal selvfølgelig hente det rigtige senere..
-  private bruger: Bruger = new Bruger('s123456', 's123456@student.dtu.dk',
+  public bruger: Bruger = new Bruger('s123456', 's123456@student.dtu.dk',
     '1585136654001', 'ukendt', 'demobruger', 'Dennis',
   'Demostudent', '<IKKE OFFENTLIG>', 'Tennis og programmering',
     'http://www.diplom.dtu.dk/');
 
-  private testData: JSON;
+  public testData: JSON;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<any>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/users/19').subscribe
+    const headers = {Username: 's180077', Password: '123'}
+    this.http.get<any>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/users/19',
+      {headers}).subscribe
     (data => {this.testData = data.total;
     });
     console.log(this.testData);

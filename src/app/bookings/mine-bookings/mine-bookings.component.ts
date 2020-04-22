81b5acd7bb042c04baae844fbab4feb7f33a9b25
isling98 <s174879@student.dtu.dk>
2020-04-22 12:11:29 +0200

    opdateret mine-bookings til at vise listen af alle bookings for en bestemt user.

--- src/app/bookings/mine-bookings/mine-bookings.component.ts
+++ src/app/bookings/mine-bookings/mine-bookings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {BookingModel} from '../booking.model';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-mybookings',
@@ -9,11 +10,10 @@ import {BookingModel} from '../booking.model';
   styleUrls: ['./mine-bookings.component.css']
 })
 export class MineBookingsComponent implements OnInit {
-  url = 'https://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/';
 
-  bookingData: Observable<BookingModel[]>;
-  bookingPost: Observable<BookingModel[]>;
   public booking: BookingModel = new BookingModel();
+
+  hentetBookings = [];
 
   constructor(private http: HttpClient) {}
 
@@ -32,50 +32,36 @@ export class MineBookingsComponent implements OnInit {
     };
   }
 
-
-
   public fetchData() {
-    /**
-     * Man kan også benytte httpParams til at authenticate headers - dvs hvis vi kan få gjort sådan at login username
-     * sendes med i header, så kan params lettere finde de bookings der passer til login username el. lign.
-     * .
-     * .
-     * Lige nu virker params ikke ordentlig, men tænker det måske er et problem med hvordan det er sat op i REST-api'et
-     */
-
-    // get booking 34
-    this.http.get<JSON>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/bookings/34').subscribe(data => {this.booking.id = data['id'],
-      this.booking.roomId = data['roomId'], this.booking.timeblock = data['timeblock'], this.booking.username = data['username']; });
-
-    console.log(this.booking);
+    /* tslint:disable:no-string-literal */
+    // get booking for user 18
+    this.http.get<JSON>('http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082/bookings/user/18')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({...responseData[key]});
+          }
+        }
+        return postArray;
+      }))
+      .subscribe(data => {
+        /*
+        this.booking.id = data['id'],
+        this.booking.roomId = data['roomId'],
+        this.booking.timeblock = data['timeblock'],
+        this.booking.username = data['username']; });
+         */
+        console.log(data);
+        this.hentetBookings = data;
+        /* tslint:disable:no-string-literal */
+      });
   }
 
   hentBooking() {
     // tslint:disable-next-line:max-line-length
 
-
   }
-  /*
-  createBooking() {
-    /**
-     * I denne metode kan de hardcoded værdier ændres til nogle værdier, som man får fra html filen vha. data bindings
-     * så det kommer til at hænge bedre sammen.
-     */
-  /*
-    const data: Bookings = {
-      id: 5,
-      timeblock: 3,
-      roomId: 2,
-      userId: 4,
-      username: 's180077',
-      month: 11,
-      year: 2020,
-      day: 11
-    }
-    this.bookingPost = this.http.post<Bookings[]>(this.url + '/bookings', data);
-
-  }
-**/
 
   /**
    * Her kunne laves en delete-metode, som fjerne ud fra bookingens id.

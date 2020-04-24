3ed8241572485c210eb400aab9a5221a255edca9
Mads Geertsen <madsgeertsen@hotmail.com>
2020-04-24 16:48:09 +0200

    datepicker, hent dato http

--- src/app/bookings/lokale-booking/lokale-booking.component.ts
+++ src/app/bookings/lokale-booking/lokale-booking.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, NgModule, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import { MaterialModule } from './material.module';
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 
 
@@ -11,15 +13,18 @@ import { MaterialModule } from './material.module';
 })
 export class LokaleBookingComponent implements OnInit {
   recipeForm: FormGroup;
-
+  url = 'http://ec2-3-20-238-191.us-east-2.compute.amazonaws.com:8082';
+  hentetBookings = [];
+  dato: Date = new Date();
 
  @Input() Booking: {personer: number, dato: number, start: number, slut: number};
 
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.hentbooking();
   }
   showhide() {
     const click = document.getElementById('drop-content');
@@ -33,7 +38,27 @@ export class LokaleBookingComponent implements OnInit {
     const click = document.getElementById('');
   }
   // Booking
-
-
-
+  hentbooking() {
+  this.http.get<JSON>(this.url + '/bookings/' + this.dato.getDate() + '/' + this.dato.getMonth())
+    .pipe(map(responseData => {
+      const postArray = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postArray.push({...responseData[key]});
+        }
+      }
+      return postArray;
+    }))
+    .subscribe(data => {
+      /*
+      this.booking.id = data['id'],
+      this.booking.roomId = data['roomId'],
+      this.booking.timeblock = data['timeblock'],
+      this.booking.username = data['username']; });
+       */
+      console.log(data);
+      this.hentetBookings = data;
+      /* tslint:disable:no-string-literal */
+    });
+}
 }
--- src/app/bookings/lokaleoversigt/lokaleoversigt.component.ts
+++ src/app/bookings/lokaleoversigt/lokaleoversigt.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-lokale-oversigt',
@@ -6,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lokaleoversigt.component.css']
 })
 export class LokaleoversigtComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
